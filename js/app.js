e4e4b02c08d880a287423657349a4979c79cadd2
sbkhaloof <sehammalkawi92@gmail.com>
2021-04-27 13:39:19 +0300

    updat

--- js/app.js
+++ js/app.js
@@ -9,37 +9,37 @@ alert(' here are group of yes or no questions game');
 alert('lets start the game')
 alert('are you new graduatied person?')
 let answ1 = prompt('enter your answer ').toLowerCase();
-if (answ1 == 'yes') 
+if (answ1 === 'yes' || answ1==='y') 
 {
     alert('are you search for job now?')
     let jobnow = prompt('enter your answer').toLowerCase()
-    if (jobnow == 'yes') {
+    if (jobnow === 'yes'|| jobnow==='y') {
         alert('do you have a good cv?');
         let cv = prompt('enter your answer').toLowerCase()
-        if (cv == 'yes') { console.log('i have good cv') }else {alert('i do not have a good one , i will work on it');console.log('i do not have a good one , i will work on it');}
+        if (cv === 'yes'||cv==='y') { console.log('i have good cv') }else {alert('i do not have a good one , i will work on it');console.log('i do not have a good one , i will work on it');}
         alert('do you have a driving license?')
         let drive = prompt('enter your answer').toLowerCase();
-        if (drive == 'yes') { console.log('yes i have driving license.'); }else { alert('i should take driving license soon.');console.log('i should take driving license soon.'); }
+        if (drive === 'yes'|| drive==='y') { console.log('yes i have driving license.'); }else { alert('i should take driving license soon.');console.log('i should take driving license soon.'); }
         alert('do you have a linkedin account?')
         let linkedin = prompt('enter your answer').toLowerCase();
-        if (linkedin == 'yes') { console.log('yes i have alinkedin account.'); } else { alert('try to sign up and have a account');console.log('try to sign up and have a account'); }
+        if (linkedin === 'yes'||linkedin==='y') { console.log('yes i have alinkedin account.'); } else { alert('try to sign up and have a account');console.log('try to sign up and have a account'); }
     }
     else { console.log('try think in your future');alert('try think in your future'); }
 }
 else {
     alert('do you have job now?');
     let jobnow = prompt('enter your answer').toLowerCase();
-    if (jobnow == 'yes') { console.log('i am happy for this news ,good luck'); }
+    if (jobnow === 'yes'|| jobnow==='y') { console.log('i am happy for this news ,good luck'); }
     else {
         alert('do you have a good cv?');
         let cv = prompt('enter your answer').toLowerCase()
-        if (cv == 'yes') { alert('i have good cv') }else {alert('i do not have a good one , i will work on it');console.log('i do not have a good one , i will work on it');}
+        if (cv === 'yes'||cv==='y') { alert('i have good cv') }else {alert('i do not have a good one , i will work on it');console.log('i do not have a good one , i will work on it');}
         alert('do you have a driving license?')
         let drive = prompt('enter your answer').toLowerCase();
-        if (drive == 'yes') { console.log('yes i have driving license.'); }else {alert('i should take driving license'); console.log('i should take driving license soon.') ;}
+        if (drive === 'yes'||drive==='y') { console.log('yes i have driving license.'); }else {alert('i should take driving license'); console.log('i should take driving license soon.') ;}
         alert('do you have a linkedin account?')
         let linkedin = prompt('enter your answer').toLowerCase();
-        if (linkedin == 'yes') { console.log('yes i have alinkedin account.'); }
+        if (linkedin === 'yes'||linkedin==='y') { console.log('yes i have alinkedin account.'); }
         else {alert('try to sign up and have a account'); console.log('try to sign up and have a account'); }
     }
      { console.log('try think in your future') }

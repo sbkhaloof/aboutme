5240d41d8ca62578fa1cf66f316ed5ceb87386d3
sbkhaloof <sehammalkawi92@gmail.com>
2021-05-05 08:21:15 +0300

    editing

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 'use strict'
 alert('welcome to my page , you will spent fun time here');
-console.log('welcome to my page');
 alert('what is your name?');
 let name = prompt('please enter your name');
 let familyname = prompt('please enter your family name ');
-console.log('welcome ' + name + ' ' + familyname);
 alert(' here are group of yes or no questions game');
 alert('lets start the game')
 function question1(){
@@ -16,40 +14,39 @@ if (answ1 === 'yes' || answ1 === 'y') {
     if (jobnow === 'yes' || jobnow === 'y') {
         alert('do you have a good cv?');
         let cv = prompt('enter your answer').toLowerCase()
-        if (cv === 'yes' || cv === 'y') { console.log('i have good cv') } else { alert('i do not have a good one , i will work on it'); console.log('i do not have a good one , i will work on it'); }
+        if (cv === 'yes' || cv === 'y') { alert('i have good cv') } else { alert('i do not have a good one , i will work on it'); }
         alert('do you have a driving license?')
         let drive = prompt('enter your answer').toLowerCase();
-        if (drive === 'yes' || drive === 'y') { console.log('yes i have driving license.'); } else { alert('i should take driving license soon.'); console.log('i should take driving license soon.'); }
+        if (drive === 'yes' || drive === 'y') { aleart('yes i have driving license.'); } else { alert('i should take driving license soon.');  }
         alert('do you have a linkedin account?')
         let linkedin = prompt('enter your answer').toLowerCase();
-        if (linkedin === 'yes' || linkedin === 'y') { console.log('yes i have alinkedin account.'); } else { alert('try to sign up and have a account'); console.log('try to sign up and have a account'); }
-        console.log('good luck');
+        if (linkedin === 'yes' || linkedin === 'y') { alert('yes i have alinkedin account.'); } else { alert('try to sign up and have a account'); }
+        alert('good luck');
     }
-    else { console.log('try think in your future'); alert('try think in your future'); }
+    else { alert('try think in your future'); alert('try think in your future'); }
 }
 else {
     alert('do you have job now?');
     let jobnow = prompt('enter your answer').toLowerCase();
-    if (jobnow === 'yes' || jobnow === 'y') { console.log('i am happy for this news ,good luck'); }
+    if (jobnow === 'yes' || jobnow === 'y') { alert('i am happy for this news ,good luck'); }
     else {
         alert('do you have a good cv?');
         let cv = prompt('enter your answer').toLowerCase()
-        if (cv === 'yes' || cv === 'y') { alert('i have good cv') } else { alert('i do not have a good one , i will work on it'); console.log('i do not have a good one , i will work on it'); }
+        if (cv === 'yes' || cv === 'y') { alert('i have good cv') } else { alert('i do not have a good one , i will work on it'); }
         alert('do you have a driving license?')
         let drive = prompt('enter your answer').toLowerCase();
-        if (drive === 'yes' || drive === 'y') { console.log('yes i have driving license.'); } else { alert('i should take driving license'); console.log('i should take driving license soon.'); }
+        if (drive === 'yes' || drive === 'y') { alert('yes i have driving license.'); } else { alert('i should take driving license');  }
         alert('do you have a linkedin account?')
         let linkedin = prompt('enter your answer').toLowerCase();
-        if (linkedin === 'yes' || linkedin === 'y') { console.log('yes i have alinkedin account.'); }
-        else { alert('try to sign up and have a account'); console.log('try to sign up and have a account'); }
+        if (linkedin === 'yes' || linkedin === 'y') { alert('yes i have alinkedin account.'); }
+        else { alert('try to sign up and have a account');  }
     }
-    { console.log('try think in your future') }
+    { alert('try think in your future') }
 };}
 question1();
 
 alert('now lets start with second part');
 alert('there is question takes a numerical answer');
-console.log('here is question takes a numerical answer');
 function question2 (){
 let mynum=5;
 for (let i = 1; i <= 4; i++) {
@@ -58,7 +55,7 @@ for (let i = 1; i <= 4; i++) {
     if (luckynum ===mynum) { alert( ' correct'); }
     else if (luckynum < mynum) { alert(luckynum + ' is too low'); }
     else if (luckynum > mynum) { alert(luckynum + ' is too high'); }
-} console.log('my lucky number is 5');
+} alert('my lucky number is 5');
 };
 question2();
 
@@ -71,7 +68,7 @@ for (let i = 1; i <= 4; i++) {
     if (badnum > mynum2) { alert(badnum + ' is too high'); }
     else if (badnum < mynum2) { alert(badnum + ' is too low'); }
     else if (badnum === mynum2) { alert('my bad number is 13'); }
-} console.log('my bad number is 13');
+} alert('my bad number is 13');
 };
 question3();
 alert('this is thired part of our game');
@@ -89,8 +86,8 @@ for (let attemp=1; attemp < 6; attemp++) {
     }
 }
 alert('the correct answer must be one of a, g,s,r,b,m,d');
-console.log('your score is ' + score + ' /7');
-alert('your score is ' + score + '/7');
+alert('your score is ' + score + ' /7');
+
 };
 question4();
 alert('the second question for this part');
@@ -107,13 +104,11 @@ for (let attemp=1; attemp < 6; attemp++) {
     }
 }
 alert('the correct answer must be one of 7,3,6,10,12');
-console.log('your score is ' + score1 + ' /7');
-alert('your score is ' + score1 + '/7');
-<<<<<<< HEAD
 
-=======
+alert('your score is ' + score1 + '/7');
+
+
 };
 question5();
->>>>>>> 2f1b3799f4edd17d274d060bd602728d87e8d415
 alert('thank you ' + name);
 alert('thank you for your visit');
